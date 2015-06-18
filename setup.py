e1dd7d68678ffc7ec71335317bfbf1a68dfa8f5a
Barbara Hui <barbara.hui@ucop.edu>
2015-06-18 15:14:36 -0700

    Restructure to use setuptools and unittest

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name = "UCLDC IIIF",
+    version = "0.0.1",
+    description = ("International Image Interoperability Framework (IIIF) implementation code for the UCLDC project"),
+    long_description=read('README.md'),
+    author='Barbara Hui',
+    author_email='barbara.hui@ucop.edu',
+    dependency_links=[
+        'https://github.com/ucldc/pynux/archive/master.zip#egg=pynux'
+    ],
+    install_requires=[
+        'boto',
+        'pynux',
+        'python-magic'
+    ],
+    test_suite='tests'
+)
