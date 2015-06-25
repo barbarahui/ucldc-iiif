8b13374e203c7d91f1644b91430f4ae9f7f18657
Barbara Hui <barbara.hui@ucop.edu>
2015-06-25 14:34:39 -0700

    Cleaning up a bit.

--- s3/convert.py
+++ s3/convert.py
@@ -21,19 +21,18 @@ class Convert(object):
         self.magick_convert_location = '/apps/nuxeo/pkg/bin/convert'
         self.kdu_compress_location = '/apps/nuxeo/kakadu/kdu_compress'
 
+
     def _uncompress_tiff(self, compressed_path, uncompressed_path):
-        ''' uncompress a tiff '''
-        # use tiffcp to uncompress: http://www.libtiff.org/tools.html
-        # tiffinfo ucm_dr_001_001_a.tif # gives you info on whether or not this tiff is compressed
-        # FIXME make sure tiffcp is installed - add to required packages
+        ''' uncompress a tiff using tiffcp. See http://www.libtiff.org/tools.html '''
         subprocess.check_output([self.tiffcp_location,
             "-c", "none",
             compressed_path,
             uncompressed_path])
         self.logger.info('File uncompressed. Input: {}, output: {}'.format(compressed_path, uncompressed_path))
 
+
     def _tiff_to_jp2(self, tiff_path, jp2_path):
-        ''' convert an uncompressed tiff to jp2 using kdu_compress'''
+        ''' convert a tiff to jp2 using kdu_compress. tiff must be uncompressed.'''
         # Settings recommended as a starting point by Jon Stroop. See https://groups.google.com/forum/?hl=en#!searchin/iiif-discuss/kdu_compress/iiif-discuss/OFzWFLaWVsE/wF2HaykHcd0J
         subprocess.check_output([self.kdu_compress_location,
                              "-i", tiff_path,
@@ -57,9 +56,9 @@ class Convert(object):
         
         self.logger.info('{} converted to {}'.format(tiff_path, jp2_path))
 
+
     def _tiff_to_jp2_no_jp2_space(self, tiff_path, jp2_path):
-        ''' convert an uncompressed tiff to jp2 using kdu_compress'''
-        # Settings recommended as a starting point by Jon Stroop. See https://groups.google.com/forum/?hl=en#!searchin/iiif-discuss/kdu_compress/iiif-discuss/OFzWFLaWVsE/wF2HaykHcd0J
+        ''' convert an uncompressed tiff to jp2 using kdu_compress, without the -jp2_space option specified '''
         subprocess.check_output([self.kdu_compress_location,
                              "-i", tiff_path,
                              "-o", jp2_path,
@@ -80,21 +79,16 @@ class Convert(object):
                              ])
         self.logger.info('{} converted to {}'.format(tiff_path, jp2_path))
 
-    def _jpx_to_jp2(self):
-        ''' convert jpx to jp2 '''
-        pass
 
     def _jpg_to_tiff(self, input_path, output_path):
-        ''' convert jpg to jp2 '''
+        ''' convert jpg to tiff using ImageMagick `convert`: http://www.imagemagick.org/script/convert.php '''
+        # TODO: this will try to convert anything to anything. Restrict usage?
         subprocess.check_output([self.magick_convert_location,
                          "-compress", "None",
                          input_path,
                          output_path])
         self.logger.info('{} converted to {}'.format(input_path, output_path))
         
-    def _gif_to_jp2(self):
-        ''' convert gif to jp2 '''
-        pass
 
 def main(argv=None):
     pass
